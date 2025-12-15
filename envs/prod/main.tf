880b51b030607dc58eb48f25fe0826ddedfac5f1
ahmedmohamedrafat00-cpu <ahmed.mohamed.rafat.00@gmail.com>
2025-12-15 22:55:52 +0200

    Add compute module with security groups for 3-tier architecture

--- envs/prod/main.tf
+++ envs/prod/main.tf
@@ -8,4 +8,11 @@ module "networking" {
   network_cidr = var.network_cidr
   azs          = var.availability_zones
 }
+module "compute" {
+  source = "../../modules/compute"
+
+  vpc_id          = module.networking.vpc_id
+  public_subnets  = module.networking.public_subnets
+  private_subnets = module.networking.private_subnets
+}
 
